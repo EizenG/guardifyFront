517401fd06e30181a891ab41a0c351e4612a5881
Michel Junior Delanoue <delanounemj@ept.sn>
2024-10-05 18:28:51 +0000

    Finish working with home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  @ViewChild("mainContainer") mainContainer !: ElementRef;
+  @ViewChildren("h5Tag") h5Tags !: QueryList<ElementRef>;
 
+  expandMenu(){
+    let nativeHtmlElt = (this.mainContainer.nativeElement as HTMLElement);
+    nativeHtmlElt.classList.toggle("expandMenu");
+    this.h5Tags.forEach(item =>{
+      nativeHtmlElt.classList.contains("expandMenu") ? item.nativeElement.previousElementSibling.style.paddingLeft = "20px" : item.nativeElement.previousElementSibling.style.paddingLeft = "0"; 
+    });
+    /*-----------------------------------*/
+    setTimeout(() =>{
+      this.h5Tags.forEach(item => {
+        (nativeHtmlElt.classList.contains("expandMenu")) ? (item.nativeElement as HTMLElement).style.opacity = "1" : (item.nativeElement as HTMLElement).style.opacity = "0";
+        item.nativeElement.hidden = (nativeHtmlElt.classList.contains("expandMenu")) ? false : true;
+      });
+       
+    },(nativeHtmlElt.classList.contains("expandMenu")?400:50));
+
+  
+  }
+
+  
 }
