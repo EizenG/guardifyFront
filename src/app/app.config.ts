873b8d08639af0ed9f581f7ced5a733572e05df4
Michel Junior Delanoue <delanounemj@ept.sn>
2024-10-19 15:51:42 +0000

    "Moved Firebase config values to external config object"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,15 +7,16 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { config } from '../../firebaseConfig';
 
 let firebaseConfig = {
-  "projectId": "guardify-backend",
-  "appId": "1:1086957650871:web:866060f9622acee4649e32",
-  "storageBucket": "guardify-backend.appspot.com",
-  "apiKey": "AIzaSyClGUs8mzCxHj1M_QXOaRGkzSKS8_RIqhs",
-  "authDomain": "guardify-backend.firebaseapp.com",
-  "messagingSenderId": "1086957650871",
-  "measurementId": "G-NCSZ5XEL52"
+  "projectId": config.projectId,
+  "appId": config.appId,
+  "storageBucket": config.storageBucket,
+  "apiKey": config.apiKey,
+  "authDomain": config.authDomain,
+  "messagingSenderId": config.messagingSenderId,
+  "measurementId": config.measurementId
 }
 
 export const appConfig: ApplicationConfig = {
